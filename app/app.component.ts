69602689d684f252f4adeb948b1e85bb4bffab87
3158 <3158@asdasda.com>
2016-11-24 07:13:07 -0300

    Alterado Titulo

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // template: `<h1>Hello {{name}}</h1>`
+  template: '<h1>Hello {{title}}</h1> <h2>{{hero}} details</h2>'
 })
-export class AppComponent  { name = 'PIRILAMPs'; }
+export class AppComponent  {
+    title = 'Tour of heroes';
+    hero = 'windstorms'; 
+  }
